56105b834d1480133583fc047a1e1e0aac0e0b38
saketpab <saketp73@gmail.com>
2026-08-21 22:53:45 -0400

    emthods practice (HTTP)

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ tasks = [
     },
     {
         "id": 2,
-        "title": "Practice REST APIs",
-        "completed": True
+        "title": "flask",
+        "completed": False
     }
 ]
 
@@ -23,8 +23,25 @@ def home():
 
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
+
+    completed = request.args.get("completed")
+    title = request.args.get("title")
+    res = tasks
+
+    if completed:
+        if completed.lower() not in ["true", "false"]:
+            return jsonify({
+                "error": "Invalid value for 'completed' parameter. Use 'true' or 'false'."
+            }), 400
+
+        completed_bool = completed.lower() == "true"
+        res = [task for task in res if task["completed"] == completed_bool]
+
+    if title:
+        res = [task for task in res if task['title'] == title]
+
     return jsonify({
-        "tasks": tasks
+        "tasks": res
     })
 
 @app.route("/tasks/<int:task_id>", methods=["GET"])
@@ -39,6 +56,109 @@ def get_task(task_id):
     return jsonify({
         "error": "Task not found"
     }), 404 
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+
+    data = request.get_json(silent=True)
+
+    if data is None:
+        return jsonify({
+            "error": "JSON body is required"
+        }), 400
+
+    if "title" not in data:
+        return jsonify({
+            "error": "Missing 'title' in request data"  
+    }), 400
+
+    if not isinstance(data["title"], str):
+        return jsonify({
+            "error": "title must be a string"
+    }), 400
+
+    if data["title"].strip() == "":
+        return jsonify({
+            "error": "title cannot be empty"
+    }), 400
+
+    completed = data.get("completed", False)
+
+    if not isinstance(completed, bool):
+        return jsonify({
+            "error": "completed must be a boolean"
+    }), 400
+
+
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": data["title"],
+        "completed": completed
+    }
+
+    tasks.append(new_task)
+
+    return jsonify(new_task), 201
+@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+
+            return jsonify({
+                "message": "Task deleted"
+            }), 200
+
+    return jsonify({
+        "error": "Task not found"
+    }), 404
+
+@app.route("/tasks/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+
+    data = request.get_json(silent=True)
+
+    if "title" not in data or "completed" not in data:
+        return jsonify({
+            "error": "title and completed are required"
+        }), 400
+    for task in tasks:
+        if task["id"] == task_id:
+
+            task["title"] = data["title"]
+            task["completed"] = data["completed"]
+
+            return jsonify(task), 200
+    return jsonify({
+        "error": "Task not found"
+    }), 404
+
+@app.route("/tasks/<int:task_id>", methods=["PATCH"])
+def patch_task(task_id):
+    data = request.get_json(silent=True)
+
+    if not data:
+        return jsonify({
+            "error": "No data provided"
+        }), 400
+
+    for task in tasks:
+        if task["id"] == task_id:
+
+            if "title" in data:
+                task["title"] = data["title"]
+            if "completed" in data:
+                task["completed"] = data["completed"]
+
+            return jsonify(task), 200
+
+    return jsonify({
+        "error": "Task not found"
+    }), 404
+
+
+
 
 
 if __name__ == "__main__":
